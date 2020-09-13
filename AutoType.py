bdcf598da4db5f203c775f4dffe830d5a670db01
EraserHead <63048447+saikat0326@users.noreply.github.com>
2020-09-13 21:45:31 +0530

    Update AutoType.py

--- AutoType.py
+++ AutoType.py
@@ -11,13 +11,27 @@ import sys
 import time
 import os
 
-#Default Values
+# Default Values
 time_speed = 0.5
 loop = True    
-letter_status = "DISABLE"
+ch2 = 77
+temp2 = False
 
-while loop == True:
-    print(r"""
+
+class Settings:
+    
+    def AppendList(self, x):
+        len1 = len(x)
+        dummy_list = []
+        for i in range(0, len1):
+            for j in x[i] [::-1]:
+                dummy_list.append(j)
+            if i < len1 - 1:
+                dummy_list.append(" ")
+        return dummy_list
+    
+    def Developer(self):
+        print(r"""
  ________  ________  ___  ___  __    ________  _________   
 |\   ____\|\   __  \|\  \|\  \|\  \ |\   __  \|\___   ___\ 
 \ \  \___|\ \  \|\  \ \  \ \  \/  /|\ \  \|\  \|___ \  \_| 
@@ -29,7 +43,65 @@ while loop == True:
 
                                                                                                                                                                                       
             """)
+    
+    def TextSettings(self):
+        
+        print("Settings:")
+        print(" 33 to Change Typing Speed")
+        print(" 44 to Change Output Text Format")
+        print(" 55 to Back")
+    
+        ch1 = int(input("\n>>>>> "))
+    
+        if ch1 == 33:
+            global time_speed
+            print("The Typing Speed is ", time_speed, "(Default : 0.5) [Range : 0 < x < 10]")
+            print("Enter The Typing Speed")
+            time_speed = float(input(">>>>> "))
+            
+            clear = lambda: os.system('cls')
+            clear()
+    
+        elif ch1 == 44:
+            clear = lambda: os.system('cls')
+            clear()
+            
+            add.Developer()
+            
+            print()
+            print(" 66 to Change Every Character to Uppercase")
+            print(" 77 to Change Every First Char. Of Word To Uppercase")
+            print(" 88 to Disable Over-writing of Characters")
+            global ch2
+            ch2 = int(input(">>>>>> "))
+    
+        elif ch1 == 55:
+            global temp2
+            temp2 = False
+            clear = lambda: os.system('cls')
+            clear()
+            
+        else:
+            print("\nInvalid Option")
+            clear = lambda: os.system('cls')
+            clear()
+            
+        return temp2
+    
+    def CapitalizeWords(self, x):
+        words = x.split()
+     
+        for x in range(len(words)):
+            words[x] = words[x] [::-1]
+            words[x] = words[x].capitalize() 
+    
+        return words
 
+add = Settings()
+
+while loop == True:
+    add.Developer()
+        
     print("Enter:")
     print(" 11 to Start")
     print(" 22 for Settings")
@@ -41,19 +113,32 @@ while loop == True:
 
     if ch == 11:
         name = input("Enter Sentence : ") [::-1]
-        if letter_status == "ENABLE":
+        t = len(name)
+        
+        if ch2 == 66:
             name = name.upper()
+            list_name = list(name)
+        
+        elif ch2 == 77:
+            name = name.lower()
+            list_name = add.CapitalizeWords(name)
+            list_name = add.AppendList(list_name)
+        
+        elif ch2 == 88:
+            list_name = list(name)
+        
         print()
         clear = lambda: os.system('cls')
         clear()
-        t = len(name)
-        list_name = list(name)
+        
+        add.Developer()
         while t:
             mins, secs = divmod(t, 60)
             sys.stdout.write(list_name[t - 1])
             sys.stdout.flush()
             time.sleep(time_speed)
             t -= 1
+            
         print("\n")
         temp = input("\nPress [Enter] to Continue...")
    
@@ -62,19 +147,13 @@ while loop == True:
         exit(0)
 
     elif ch == 22:
+        clear = lambda: os.system('cls')
+        clear()
+        
+        add.Developer()
         checkSettings = True
         while checkSettings == True:
-            print("The Typing Speed is ", time_speed, "(Default : 1.00) [Range : 0 < x < 10]")
-            print("Enter The Typing Speed")
-            time_speed = float(input(">>>>> "))
-            print("\nCaptialize Each Letter :", letter_status)
-            letter_status = input("Enable/Disable >>>> ").upper()
-            
-            if (letter_status == "ENABLE" or letter_status == "DISABLE") and time_speed > 0.0 and time_speed < 10.0:
-                checkSettings = False
-            else:
-                print("\nInvalid Option !\n")
-                checkSettings = True 
+            checkSettings = add.TextSettings() 
     
     else:
         temp = input("Invalid Option Press [Enter] to Try Again...")
